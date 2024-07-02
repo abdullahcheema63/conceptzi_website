69a3568a66139440339057dae14d18bb3f05db46
Abdullah Cheema <abdullahcheema.macmads@gmail.com>
2024-07-02 21:37:17 +0500

    completely transferred

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://conceptzi.com/</homePageLink>
+		<homePageLink>https://conceptzi.web.app/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://conceptzi.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://conceptzi.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://conceptzi.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://conceptzi.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://conceptzi.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://conceptzi.web.app/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://conceptzi.web.app/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://conceptzi.web.app/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://conceptzi.web.app/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://conceptzi.web.app/wp-json/" />
 			</apis>
 	</service>
 </rsd>
